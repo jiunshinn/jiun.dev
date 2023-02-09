b64eb9163115abd138e792998856959d8994bf41
jiun-dev <jiun.dev@gmail.com>
2023-02-09 13:38:19 +0900

    site map

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,48 @@
+const EXTERNAL_DATA_URL = 'https://www.jiun.me/posts';
+
+function generateSiteMap(posts : any) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://www.jiun.me/</loc>
+     </url>
+     <url>
+       <loc>https://www.jiun.me/about</loc>
+     </url>
+     ${posts
+       .map(({ id } : any) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+       </url>
+     `;
+       })
+       .join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res } : any) {
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(EXTERNAL_DATA_URL);
+  const posts = await request.json();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(posts);
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
