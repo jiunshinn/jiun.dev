423490d4d3888ba8c8ade0de0c9c3a5519b4d2a9
jiun-dev <jiun.dev@gmail.com>
2023-02-09 14:37:52 +0900

    sitemap update

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,44 +1,34 @@
-//pages/sitemap.xml.js
+import axios from "axios";
+
+
 const EXTERNAL_DATA_URL = 'https://www.jiun.me/posts';
 
-function generateSiteMap(posts : any) {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>https://jsonplaceholder.typicode.com</loc>
-     </url>
-     <url>
-       <loc>https://jsonplaceholder.typicode.com/guide</loc>
-     </url>
-     ${posts
-       .map(({ id } : any) => {
-         return `
-       <url>
-           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
-       </url>
-     `;
-       })
-       .join('')}
-   </urlset>
- `;
-}
+const generateSiteMap = (slugs: string[]) => `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${slugs
+          .map((slug) => {
+            return `
+                <url>
+                    <loc>${`https://www.jiun.me/${slug}`}</loc>
+                </url>
+            `;
+          })
+          .join('')}
+    </urlset>
+`;
 
 function SiteMap() {
   // getServerSideProps will do the heavy lifting
 }
 
 export async function getServerSideProps({ res } : any) {
-  // We make an API call to gather the URLs for our site
-  const request = await fetch(EXTERNAL_DATA_URL);
-  const posts = await request.json();
 
-  // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts);
+  const { data: { posts = [] } = {} } = await axios.get(`/api/posts`);
+  const allPages = [...posts.map(({ _id } : any ) => `posts/${_id}/`), ...['', 'about']];
 
   res.setHeader('Content-Type', 'text/xml');
-  // we send the XML to the browser
-  res.write(sitemap);
+  res.setHeader('Cache-Control', 'public, s-maxage=1200, stale-while-revalidate=600');
+  res.write(generateSiteMap(allPages));
   res.end();
 
   return {
