b16ef78cd97b636c1ebd14d861b73d34677d1829
jiun-dev <jiun.dev@gmail.com>
2023-02-14 15:18:26 +0900

    study, portfolio index

---
+++ src/pages/study/index.tsx
@@ -0,0 +1,13 @@
+import Layout from 'components/layout'
+
+export default function Study() {
+  return (
+    <Layout>
+      <section >
+        <h2>Study</h2>
+        
+      </section>
+    </Layout>
+  )
+}
+
